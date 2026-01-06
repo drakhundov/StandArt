ecfcffcbd0877cd6f0b78ebf76c44fccc1769fa1
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2026-01-06 20:30:10 +0300

    Save files via file manager.

--- filemng.py
+++ filemng.py
@@ -1,6 +1,10 @@
+import os
+import pickle
 from typing import Literal, Optional
 
 from tkinter import filedialog
+
+from picture import Picture
 
 
 class OSFileManager:
@@ -34,6 +38,37 @@ class OSFileManager:
         else:
             print("File path has already been established.")
             return None
+
+    def save(self, pic_obj: Picture, is_new_path: bool):
+        path: str
+        is_valid_path: bool
+        if (self.get_path() is None or is_new_path) and (
+            (path := self.select_path("save")) is not None and
+            (is_valid_path := os.path.exists(path))
+        ):
+            self.set_path(path)
+        elif not is_valid_path:
+            raise OSError("[OSFileManager:save] Invalid path: ", path)
+        else:
+            return
+
+        extension = os.path.splitext(path)[1]
+        if extension == ".art":
+            with open(path, "wb") as f:
+                pickle.dump(pic_obj.get(), f)
+
+        self.set_saved(True)
+
+        # elif extension == ".png":
+        #     form_scale = self.ui.form_scale()
+        #     canvas_scale = self.ui.canvas_scale()
+
+        #     x1 = form_scale[0] + canvas_scale[0]
+        #     y1 = form_scale[1] + canvas_scale[1]
+        #     x2 = x1 + self.ui.canvas_width()
+        #     y2 = y1 + self.ui.canvas_height()
+
+        #     ImageGrab.grab().crop((x1, y1, x2, y2)).save(self.file.get_path())
 
     def get_path(self) -> Optional[str]:
         if self.path != "":
