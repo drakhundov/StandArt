28f20ee6e54a5f2546d5fb546a371428cf3dc24c
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2026-01-07 20:40:14 +0300

    Basic GUI constructor.

--- gui.py
+++ gui.py
@@ -1,95 +1,119 @@
 from tkinter import *
 import os
+from typing import Literal, Callable, List
 
-class GUI:
+
+class GUIConstructor:
     def __init__(self):
-        self.__form__ = Tk()
-        self.__form__.geometry('800x550')
-        self.__form__.title('StandArt')
-        self.__form__.resizable(0, 0) # user couldn't change size of window
+        self.tk = Tk(useTk=True)
+        # self.tk.geometry("1000x550")
+        self.tk.title("StandArt")
+        # self.__form__.resizable(0, 0)  # user couldn't change size of window
 
-        if os.name != 'posix':
-        	self.__form__.iconbitmap('icon.ico')
+        if os.name != "posix":
+            self.tk.iconbitmap("icon.ico")
 
         self.set_menu()
         self.set_ui()
 
-    
     def set_menu(self):
-        self.__main_menu__ = Menu(self.__form__)
-        self.__form__.configure(menu = self.__main_menu__)
+        self.main_menu = Menu(self.tk)
+        self.tk.configure(menu=self.main_menu)
 
-        self.__options_item__ = Menu(self.__main_menu__)
-        self.__main_menu__.add_cascade(label = 'File', menu = self.__options_item__)
+        self.file_opts_submenu = Menu(self.main_menu)
+        self.main_menu.add_cascade(label="File", menu=self.file_opts_submenu)
 
-    
     def set_ui(self):
-        self.canvas = Canvas(self.__form__, bg = 'white', width = self.__form__.winfo_width () - 50, height = self.__form__.winfo_height () - 120)
-        self.canvas.grid(row = 1, column = 0, columnspan = 10, padx = 5, pady = 5, sticky = E+W+S+N)
+        self.canvas = Canvas(
+            self.tk,
+            bg="white",
+            width=self.tk.winfo_screenwidth() * 0.5,
+            height=self.tk.winfo_screenheight() * 0.5,
+        )
+        self.canvas.grid(
+            row=1, column=0, columnspan=10, padx=5, pady=5, sticky=E + W + S + N
+        )
 
-        self.brush_size = Scale(self.__form__, from_ = 0, to = 100, orient = HORIZONTAL)
-        self.brush_size.grid(row = 0, column = 8)
-        
-        self.clear_button = Button(self.__form__, text = 'Clear', width = 10, bg = 'white')
-        self.clear_button.grid(row = 2, column = 8)
- 
-        self.erase_button = Button(self.__form__, text = 'Erase', width = 10, bd = 1, bg = 'white')
-        self.erase_button.grid (row = 2, column = 9)
+        self.brush_size_scale = Scale(self.tk, from_=0, to=100, orient=HORIZONTAL)
+        self.brush_size_scale.grid(row=0, column=8)
 
+        self.clear_button = Button(self.tk, text="Clear", width=10, bg="white")
+        self.clear_button.grid(row=2, column=8)
 
-    def set_colors(self, colors, set_color_func):
-        self.__colors__ = []
+        self.erase_button = Button(self.tk, text="Erase", width=10, bd=1, bg="white")
+        self.erase_button.grid(row=2, column=9)
+
+    def set_color_buttons(self, colors: List[str], set_color_func: Callable):
+        self.color_select_btn_lst = []
         for color in colors:
-            self.__colors__.append(Button(self.__form__, bg = color, bd = 1, width = 5, height = 3, command = lambda color=color: set_color_func(color)))
-            self.__colors__[-1].grid(row = 0, column = colors.index(color))
+            # Use labels instead of buttons since MacOS
+            # controls button style for consistency.
+            new_lbl = Label(
+                self.tk,
+                bg=color,
+                highlightthickness=0,
+                bd=1,
+                width=5,
+                height=3,
+                # command=lambda color=color: set_color_func(color),
+            )
+            self.color_select_btn_lst.append(new_lbl)
+            new_lbl.bind("<Button-1>", lambda e, c=color: set_color_func(c))
+            new_lbl.grid(row=0, column=colors.index(color))
 
-
-    def add_option(self, name, command):
-        self.__options_item__.add_command(label = name, command = command)
-
-
-    def keyboard(self, main_key, keys, command):
-        for key in keys:
-            self.__form__.bind(f'<{main_key}-{key}>', command)
-    
-
-    def mouse_motion(self, command):
-        self.canvas.bind ('<B1-Motion>', command)
-        self.canvas.bind ('<Button-1>', command)
-    
-
-    def clear(self, command):
-        self.clear_button.config(command = command)
-
-
-    def erase(self, command):
-        self.erase_button.config(command = command)
-
-
-    def on_closing(self, command):
-        self.__form__.protocol('WM_DELETE_WINDOW', command)
-
+    def set_callback(
+        self,
+        command: Literal[
+            "erase",
+            "clear",
+            "mouse_motion",
+            "on_closing",
+            "keyboard_conf",
+            "save_file",
+            "open_file",
+            "save_as",
+        ],
+        callback: Callable,
+        *args,
+        **kwargs,
+    ):
+        match command:
+            case "erase":
+                self.erase_button.config(command=callback)
+            case "clear":
+                self.clear_button.config(command=callback)
+            case "mouse_motion":
+                self.canvas.bind("<B1-Motion>", callback)
+                self.canvas.bind("<Button-1>", callback)
+            case "on_closing":
+                self.tk.protocol("WM_DELETE_WINDOW", callback)
+            case "keyboard_conf":
+                keys = kwargs["keys"]
+                main_key = kwargs["main_key"]
+                for key in keys:
+                    self.tk.bind(f"<{main_key}-{key}>", callback)
+            case "save_file":
+                self.file_opts_submenu.add_command(label="Save", command=callback)
+            case "open_file":
+                self.file_opts_submenu.add_command(label="Open", command=callback)
+            case "save_as":
+                self.file_opts_submenu.add_command(label="Save As", command=callback)
 
     def update(self):
-        self.__form__.update()
-        self.__form__.update_idletasks()
-
+        self.tk.update()
+        self.tk.update_idletasks()
 
     def destroy(self):
-        self.__form__.destroy()
-    
+        self.tk.destroy()
 
-    def form_scale(self):
-        return (self.__form__.winfo_rootx(), self.__form__.winfo_rooty())
-    
+    def root_position(self):
+        return (self.tk.winfo_rootx(), self.tk.winfo_rooty())
 
-    def canvas_scale(self):
+    def root_position(self):
         return (self.canvas.winfo_x(), self.canvas.winfo_y())
-    
 
     def canvas_width(self):
         return self.canvas.winfo_width()
-    
 
     def canvas_height(self):
         return self.canvas.winfo_height()
